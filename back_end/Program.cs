9d53fa2dae42cdc5a4001a0570f1b1f88f58c5e8
ander0code <maxxd814@gmail.com>
2025-05-23 12:24:47 -0500

    version(init)0.013

--- back_end/Program.cs
+++ back_end/Program.cs
@@ -24,9 +24,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
     .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
-    .AddJsonFile("appsettings.Local.json", optional: false, reloadOnChange: true) // Cambiado a false para asegurarse de que se cargue
-    .AddEnvironmentVariables() 
-    .AddUserSecrets<Program>(optional: true); 
+    .AddJsonFile("appsettings.Local.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables()
+    .AddUserSecrets<Program>(optional: true);
 
 
 builder.Services.AddAppSettings();
