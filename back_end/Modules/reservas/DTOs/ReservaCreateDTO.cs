b5057188146fa9b7c74ba7191cb1436d42fc98b4
Angel <facundocarp679@gmail.com>
2025-05-29 11:29:21 -0500

    version(ini) 0.030: agregando validaciones

--- back_end/Modules/reservas/DTOs/ReservaCreateDTO.cs
+++ back_end/Modules/reservas/DTOs/ReservaCreateDTO.cs
@@ -7,7 +7,10 @@ namespace back_end.Modules.reservas.DTOs
         public string? Estado { get; set; }
         public decimal? PrecioTotal { get; set; }
         public string? ClienteId { get; set; }
-        // Cambiar TipoEventoId por TipoEventoNombre
+        // Campos para crear cliente automáticamente si no existe
+        public string? NombreCliente { get; set; }
+        public string? CorreoElectronico { get; set; }
+        public string? Telefono { get; set; }
         public string? TipoEventoNombre { get; set; }
         public Guid? ServicioId { get; set; }
         public double? PrecioAdelanto { get; set; }
